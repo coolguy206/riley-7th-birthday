3b269a0308113776da3c1873c4c00285c67901da
coolguy206 <davidthanphilom@msn.com>
2022-04-30 19:41:46 -0700

    js/scripts.js still coding main

--- js/scripts.js
+++ js/scripts.js
@@ -41,37 +41,108 @@ $(document).ready(function() {
     $('.sending').show();
     var $inputs = $('form :input');
     console.log($inputs);
-    var arr = [];
+    var arr = [
+      ['date', 'adult1', 'adult1-swim', 'adult2', 'adult2-swim', 'child1', 'child1-swim', 'child2', 'child2-swim', 'child3', 'child3-swim', 'child4', 'child4-swim']
+    ];
+
     var date = new Date().toDateString();
-    $inputs.map(function(i, val) {
-      var value = val.value;
-      var type = val.type;
-      var name = val.name;
-      var checked = val.checked
-      var obj = {
-        value: value,
-        type: type,
-        name: name,
-      };
-      // console.log(obj);
-      if (type == 'radio') {
-        var inputVal = $(val).closest('.second').prev().find('input').val();
-        if (inputVal !== '') {
-          if (checked == true) {
-            arr.push(obj);
+    /*
+        $inputs.map(function(i, val) {
+          var value = val.value;
+          var type = val.type;
+          var name = val.name;
+          var checked = val.checked
+          var arrVal = [
+            value: value,
+            type: type,
+            name: name,
+          ];
+          // console.log(obj);
+          if (type == 'radio') {
+            var inputVal = $(val).closest('.second').prev().find('input').val();
+            if (inputVal !== '') {
+              if (checked == true) {
+                arr.push(arrVal);
+              }
+            }
+
+          } else if (type == 'text') {
+            if (value !== '') {
+              arr.push(arrVal);
+            }
           }
-        }
 
-      } else if (type == 'text') {
-        if (value !== '') {
-          arr.push(obj);
-        }
-      }
-
-    });
-
+        });
+    */
+    // .serialize()
     console.log(arr);
     console.log(date);
+
+    var range = `Sheet1!A1:M1`;
+    var rangeURL = encodeURIComponent(range);
+
+    var obj = {
+      "majorDimension": "ROWS",
+      "range": range,
+      "values": arr,
+    };
+
+    obj = JSON.stringify(obj);
+    console.log(typeof obj);
+    console.log(obj);
+
+    // {
+    //   "range": "Sheet1!A1:D5",
+    //   "majorDimension": "ROWS",
+    //   "values": [
+    //     ["Item", "Cost", "Stocked", "Ship Date"],
+    //     ["Wheel", "$20.50", "4", "3/1/2016"],
+    //     ["Door", "$15", "2", "3/15/2016"],
+    //     ["Engine", "$100", "1", "3/20/2016"],
+    //     ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
+    //   ],
+    // }
+
+    // var x =`https://sheets.googleapis.com/v4/spreadsheets/1UkoueEhKq7ogT9Z557tX_tTHAC1qeLaw0zFo5j3CcMo/values/Sheet1!A1%3AM1?responseDateTimeRenderOption=SERIAL_NUMBER&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=USER_ENTERED&key=[YOUR_API_KEY] HTTP/1.1`;
+
+
+    console.log(`refresh test`);
+
+    var key = `AIzaSyDFdm54gGbCpuu8rJKAa__QvI1slX8fy0Y`;
+    var clientId = `684411187765-8hbqvl6ar7s5gvc1ov882pl5oenitvoc.apps.googleusercontent.com`;
+    var accessToken = `ya29.A0ARrdaM_u4zaOxZl1W4rTLeB8-5__oL08Cv4-ndx42ck6ZUn236wDmKlhrr9uVVlCqN0WKdalWDQ447C77QBDbqL2pLTnhTLe4vusLLCK1pfJgXrL3kAQ7yudHAWrmVT-EXIqigYG02bcu7yHx7vjnDLppljw`;
+    var refreshAccessToken = `1//04BwYuIW4jzkdCgYIARAAGAQSNwF-L9IrADxMgJGStfKhYBIwi_PygwY4m13GeCi7vjg0br0vgZv8KR9elhoecjDkbiVsBK92wq4`;
+    var clientSecret = `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`;
+    var sheetId = `1UkoueEhKq7ogT9Z557tX_tTHAC1qeLaw0zFo5j3CcMo`;
+    var url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${rangeURL}?access_token=${accessToken}&valueInputOption=USER_ENTERED`;
+    console.log(url);
+
+    // $.post(url, obj, function(data) {
+    //   console.log('goole sheets POST');
+    //   console.log(data);
+    // });
+
+    var refreshURL = `https://oauth2.googleapis.com/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshAccessToken}&grant_type=refresh_token`;
+
+    // $.post(refreshURL, function(data) {
+    //   console.log('goole api refresh access token');
+    //   console.log(data);
+    // });
+
+    $.ajax({
+      url: url,
+      method: 'PUT',
+      data: obj,
+      headers: {
+        "Authorization": `OAuth ${accessToken}`,
+      },
+      dataType: 'json',
+      success: function(result) {
+        // Do something with the result
+        console.log(result);
+      }
+    });
+
 
   });
 
